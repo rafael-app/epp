c368d2be808a963b2423c26985ef7a39a7d6a750
rafael-app <code@rafael.app>
2023-10-30 21:59:34 -0700

    Initial Commit

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+module.exports = function (eleventyConfig) {
+
+	// Copy `img/` to `_site/img`
+	eleventyConfig.addPassthroughCopy("img");
+
+	// Copy the stylesheet specifically
+	eleventyConfig.addPassthroughCopy("./src/style.css");
+	eleventyConfig.addPassthroughCopy("./src/uikit.min.css");
+	eleventyConfig.addPassthroughCopy("./src/uikit.min.js");
+	return {
+		dir: {
+			input: "src",
+			output: "public",
+		},
+	};
+
+	// Copy any .jpg file to `_site`, via Glob pattern
+	// Keeps the same directory structure.
+	eleventyConfig.addPassthroughCopy("**/*.jpg");
+	eleventyConfig.addPassthroughCopy("**/*.svg");
+	eleventyConfig.addPassthroughCopy("**/*.mp4");
+};
